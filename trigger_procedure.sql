ab4e75c7efef6c75803d3988a82933092e5e535b
TranTriDat2002 <96716352+TranTriDat2002@users.noreply.github.com>
2022-11-27 01:54:43 +0700

    Done store procedure

--- trigger_procedure.sql
+++ trigger_procedure.sql
@@ -322,6 +322,83 @@ BEGIN
 		)
 		ORDER BY sit.No_of_votes; 
     END IF;
+    IF (InEp = 4) THEN
+    	SELECT s.Ssn_trainee, s.No_of_votes
+        FROM stageincludetrainee s
+        WHERE (s.SYear, s.Ep_No) = (InYear, InEp)
+        ORDER BY s.No_of_votes DESC
+        LIMIT 10;
+    END IF;
+    IF (InEp = 5) THEN
+    	SELECT s.Ssn_trainee, s.No_of_votes
+        FROM stageincludetrainee s
+        WHERE (s.SYear, s.Ep_No) = (InYear, InEp)
+        AND s.Stage_No IN(
+            SELECT stage.Stage_No
+            FROM stage
+            WHERE (stage.Syear, stage.Ep_No, stage.Is_Group) = (InYear, InEp, 0)
+        )
+        ORDER BY s.No_of_votes DESC
+        LIMIT 5;
+    END IF;
+END;
+//
+DELIMITER ;
+
+-- 2. Create a procedure/function to retrieve the result of a trainee in a season --
+DELIMITER //
+CREATE OR REPLACE PROCEDURE trainee_result
+(InYear year, InSsn decimal(12,0))
+BEGIN
+    SELECT a.Ep_no as 'Episode', b.Score as 'Num of votes/ avg score'
+    FROM(
+        SELECT 1 as Ep_no, NULL as 'Score'
+        UNION ALL
+        SELECT 2 as Ep_no, NULL as 'Score'
+        UNION ALL
+        SELECT 3 as Ep_no, NULL as 'Score'
+        UNION ALL 
+        SELECT 4 as Ep_no, NULL as 'Score'
+        UNION ALL
+        SELECT 5 as Ep_no, NULL as 'Score'
+    ) AS a
+    LEFT JOIN(
+        SELECT 1 as Ep_no, AVG(mvt.Score) as 'Score'
+        FROM mentorvaluatetrainee mvt
+        WHERE (mvt.Syear, mvt.Ssn_trainee) = (InYear, InSsn)
+
+        UNION 
+
+        SELECT 2 as Ep_no, sit.No_of_votes as 'Score'
+        FROM stageincludetrainee sit
+        WHERE (sit.SYear, sit.Ep_No, sit.Ssn_trainee) = (InYear, 2, InSsn)
+
+        UNION
+
+
+        SELECT 3 as Ep_no, sit.No_of_votes as 'Score'
+        FROM stageincludetrainee sit
+        WHERE (sit.SYear, sit.Ep_No, sit.Ssn_trainee) = (InYear, 3, InSsn)
+
+        UNION
+
+
+        SELECT 4 as Ep_no, sit.No_of_votes as 'Score'
+        FROM stageincludetrainee sit
+        WHERE (sit.SYear, sit.Ep_No, sit.Ssn_trainee) = (InYear, 4, InSsn)
+
+        UNION
+
+        SELECT 5 as Ep_no, sit.No_of_votes as 'Score'
+        FROM stageincludetrainee sit
+        WHERE sit.Ssn_trainee = InSsn
+        AND sit.Stage_No IN(
+            SELECT s.Stage_No
+            FROM stage s
+            WHERE (s.Syear, s.Ep_No, s.Is_Group) = (InYear, 5, 0)
+        )
+    ) as b
+    ON a.Ep_no = b.Ep_no;
 END;
 //
 DELIMITER ;
